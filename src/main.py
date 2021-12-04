fc9f60a13e350569fbfe6a045b804f876fdd2ff0
Pedro <pedrocaetanoaraujopc@gmail.com>
2021-12-04 16:31:24 -0300

    Merge the grabcut with the edge detection

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from cv2 import cv2
+from cv2 import cv2, imread
 import numpy as np
+import os
 
 from remove_background import remove_background
 
 if __name__ == "__main__":
-    image = cv2.imread("../images/test_cow.jpeg")
-    image_mask = cv2.imread("../images/test_mask.png", 0)
+    image = cv2.imread(r"C:\Users\pedro\workspace\cow-bcs-classification\images\test_cow.jpeg")
+    image_mask = cv2.imread(r"C:\Users\pedro\workspace\cow-bcs-classification\images\test_mask.png", 0)
 
     image_no_bgd = remove_background(image, image_mask)
 
-    cv2.imwrite("../output/images_without_background/test_cow.jpeg", image_no_bgd)
+    cv2.imwrite(r"C:\Users\pedro\workspace\cow-bcs-classification\output\images_without_background\test_cow.jpeg", image_no_bgd)
