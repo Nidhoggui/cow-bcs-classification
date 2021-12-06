4678f9a1479abf9f7d87c4b305bc73afa6e770cf
Nidhoggui <guilhermemelquiades42@gmail.com>
2021-12-06 08:51:03 -0300

    Adicionando mascaras e interpolacao/redimensionamento para as mascaras

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-from cv2 import cv2
+from cv2 import cv2, imread
 import numpy as np
+import os
 
 from remove_background import remove_background
+from interpolacao import redimensiona
 
 if __name__ == "__main__":
-    image = cv2.imread("../images/test_cow.jpeg")
-    image_mask = cv2.imread("../images/test_mask.png", 0)
+    imagemMascara = cv2.imread('..\images\mask_vaca_traseira.png', 0)
+    imagemBase = cv2.imread('..\images\test_cow.jpeg')
+
+    image_mask = redimensiona(imagemMascara, imagemBase)
+    image = cv2.imread('..\images\test_cow.jpeg')
 
     image_no_bgd = remove_background(image, image_mask)
 
-    cv2.imwrite("../output/images_without_background/test_cow.jpeg", image_no_bgd)
+    cv2.imwrite("..\output\images_without_background\test_cow.jpeg", image_no_bgd)
