eb44e2d692959091c7d878a4b83604019337df65
Gabriel Viana <gabrielalmeidavn@gmail.com>
2022-04-28 10:52:35 -0300

    add hu moments code

--- src/shape/main.py
+++ src/shape/main.py
@@ -28,19 +28,35 @@ def get_top_back_shape(image):
     return image
 
 
-def main():
-    images_path = os.path.abspath('../../images/segmented_images/ECCs/ECC 3,0/result_vaca traseira 1(perfeito_pasto).jpeg')
-    cow_tail_image = cv2.imread(images_path, 0)
+def hu_moments_with_log_transformation(image):
+    moments = cv2.moments(image)
+    hu_moments = cv2.HuMoments(moments)
 
-    blur_image = cv2.blur(cow_tail_image, (3, 3))
-    erode_image = erode(blur_image, 4)
-    erode_image[erode_image != 0] = 255
-    subtract_image = cv2.subtract(blur_image, erode_image)
+    # log transformation
+    for i in range(len(hu_moments)):
+        hu_moments[i] = -1 * np.sign(hu_moments[i]) * np.log10(np.abs(hu_moments[i]))
 
-    top_back_shape = get_top_back_shape(subtract_image)
+    return hu_moments
 
-    show_images((cow_tail_image, blur_image, subtract_image, top_back_shape))
+
+def main(images):
+    for image in images:
+        blur_image = cv2.blur(image, (3, 3))
+        erode_image = erode(blur_image, 4)
+        erode_image[erode_image != 0] = 255
+        subtract_image = cv2.subtract(blur_image, erode_image)
+
+        top_back_shape = get_top_back_shape(subtract_image)
+
+        print(hu_moments_with_log_transformation(top_back_shape).flatten())
+
+        show_images((image, blur_image, subtract_image, top_back_shape))
 
 
 if __name__ == "__main__":
-    main()
+    images_path = os.path.abspath('../../images/segmented_images/ECCs/ECC 3,0')
+    cow_tail_image_1 = cv2.imread(images_path + "/result_vaca traseira 1(perfeito_pasto).jpeg", cv2.IMREAD_GRAYSCALE)
+    cow_tail_image_2 = cv2.imread(images_path + "/result_vaca traseira 5(quase_perfeito_pasto).jpeg", cv2.IMREAD_GRAYSCALE)
+    cow_tail_image_2[cow_tail_image_2 > 150] = 0  # remove the part of the sky that was left from the background
+
+    main([cow_tail_image_1, cow_tail_image_2])
